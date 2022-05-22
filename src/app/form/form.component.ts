c2ff3bee0834913bd331c26a3a8e4714c6854fb7
kienhi123 <kien123@gmail.com>
2022-05-22 19:04:30 +0700

    Edit from

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -12,9 +13,80 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(formData:{name:string,age:string}){
-     console.log(formData)
+  inputValues = {
+    id: 0,
+    name: '',
+    age: 0,
+    email:"",
+    sdt:"",
+    avatar:''
+  };
+
+  users= [
+    {
+      id:1,
+      name:'kienhi123',
+      age:20,
+      email:'kientvph17883@fpt.edu.vn',
+      sdt:'0376517352',
+      avatar:"https://www.saga.vn/uploads/%E1%BA%A2nh%20m%C3%B4%20t%E1%BA%A3/anhmota_-how-to-take-and-edit-photos-without-hiring-a-professional.jpg"
+    },
+    {
+      id:10,
+      name:'kienhi2002',
+      age:10,
+      email:'kientvph17883@fpt.edu.vn',
+      sdt:'037659352',
+      avatar:"https://www.saga.vn/uploads/%E1%BA%A2nh%20m%C3%B4%20t%E1%BA%A3/anhmota_-how-to-take-and-edit-photos-without-hiring-a-professional.jpg"
+
+      
+    }
+  ]
+
+  onSubmit(userForm:NgForm){
+    //  console.log(formData)
+     // Tìm ra id lớn nhất
+   const userIds = this.users.map(user=>user.id).sort((a,b)=> a-b);
+  //  console.log(userIds)
+     const newId = userIds [userIds.length-1]
+
+
+if(this.inputValues.id==0){
+     // thêm dữ liệu mới vào bảng
+
+     this.users.push({
+      ...userForm.value,
+      id:newId+1
+    })
+}else{
+  const index= this.users.findIndex((user)=>user.id === this.inputValues.id)
+  if(index > -1){
+         this.users[index] = {
+           ...userForm.value,
+           id:this.inputValues.id
+         }
+  }
+}
+
+     userForm.resetForm({
+      name: '',
+      age: 0,
+      email: '',
+      sdt:'',
+      avatar:''
+    });
   }
 
+  onDelete(userId:number){
+    this.users= this.users.filter((user)=>user.id !== userId)
+  }
 
+  onEdit(userId:number){
+     // Tìm ra user  có id là userId truyền vào
+     const editUser = this.users.find((user)=> user.id === userId)
+     console.log(editUser);
+     if(editUser){
+      this.inputValues= {...editUser};
+     }
+  } 
 }
